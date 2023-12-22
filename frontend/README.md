987dda30dbd1ce12792348f5fd6e7e1f6ef27812
Austin Bennett <ausbenn@icloud.com>
2023-12-21 22:53:02 -0500

    Switched Build System to Vite, Simple BS5 UI

--- frontend/README.md
+++ frontend/README.md
@@ -1,24 +1,8 @@
-# frontend
+## How to use
 
-## Project setup
-```
+```sh
+git clone https://github.com/twbs/examples.git
+cd examples/vue/
 npm install
+npm start
 ```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ frontend/src/js/App.vue
@@ -0,0 +1,73 @@
+<!-- 
+  - reset after submit
+  - highlight selected category
+
+ -->
+
+<template>
+
+  <div class="container bg-warning p-2  m-5">
+
+
+    <div class="row p-3 m-3">
+      <div class="col">
+        <div class="form-floating">
+        <input type="text" class="form-control" placeholder="title" v-model="title">
+        <label for="floatingInput">Title</label>
+      </div>
+      </div>
+    </div>
+    
+    <div class="row p-3 m-3">
+      <div class="col btn-group">
+        <button class="btn btn-primary" @click="setType(name)" v-for="(name, index) in types" :key="index">
+          {{ name }}
+        </button>
+      </div>
+    </div>
+
+    <div class="row p-3 m-3">
+      <div class="input-group">
+        <span class="input-group-text">$</span>
+        <input type="number" v-model="price" class="form-control" aria-label="Amount (to the nearest dollar)">
+        <button type="button" class="btn btn-success" @click="printData">print data object</button>
+      </div>
+    </div>
+
+</div>
+  
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: '',
+      price: 5,
+      types: ['Groceries', 'Personal', 'Education', 'Misc'],
+      dataObject:{
+        title: 'n/a',
+        price: -1,
+        type: 'n/a',
+        date: new Date()
+      }
+    }
+  },
+  methods: {
+    sendData(){
+      this.dataObject.title = this.title
+      this.dataObject.price = this.price
+      // const jsonPayload = JSON.stringify(this.dataObject);
+    },
+    printData(){ //for logging 
+      this.dataObject.title = this.title
+      this.dataObject.price = this.price
+      console.log(this.dataObject)
+    },
+    setType(type){
+      this.dataObject.type = type
+    }
+  }
+}
+</script>
